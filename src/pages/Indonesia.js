dc4cd4893789846fb45d5981dbd57ea864802124
meika <putumeikashy27@gmail.com>
2023-10-16 22:54:48 +0800

    Button

---
+++ src/pages/Indonesia.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Indonesia extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Indonesia</h2>
+            </div>
+        )
+    }
+}
---
+++ src/pages/Programming.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Programming extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Programming</h2>
+            </div>
+        )
+    }
+}
