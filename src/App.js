b9eb432b3a837444e9fa7401ead00f494f89e66a
Tsalmanisal <109224556+justIsal@users.noreply.github.com>
2023-10-17 21:46:31 +0700

    delete App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,1 @@
-import './App.css';
-import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
-}
-
-export default App;
