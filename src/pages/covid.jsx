40c97c974c76ffffab6cda80b1300ff17bd059aa
justIsal <tsalmanisal@gmail.com>
2023-10-17 21:36:07 +0700

    update extension file js to jsx

---
+++ src/pages/covid.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Covid extends Component {
+    render() {
+        return (
+            <div>
+                <h2> Covid</h2>
+            </div>
+        )
+    }
+}
