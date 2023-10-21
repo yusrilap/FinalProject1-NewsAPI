3a0c667e6f21f9ea38337e913c97a92f6fe0c6fc
justIsal <tsalmanisal@gmail.com>
2023-10-21 09:14:40 +0700

    update

--- src/components/SectionHeader/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./style.module.css";
-
-const SectionHeader = ({ title }) => {
-    return (
-        <h2 className={styles.title}>{title}</h2>
-    );
-};
-
-export default SectionHeader;
